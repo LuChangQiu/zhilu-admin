78d0688ab6e99dee8d2524249d67d35a2a17c628
Chuck1sn <chuck1inzl@gmail.com>
2025-05-14 11:32:08 +0800

    fix repo

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -41,6 +41,10 @@ configurations {
 }
 
 repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/central")}
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+    maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
     mavenCentral()
 }
 
