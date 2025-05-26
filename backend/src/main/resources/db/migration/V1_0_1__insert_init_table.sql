e0d5723b5b8c758eb3e93fab280a34b6d877e2a4
Chuck1sn <chuck1inzl@gmail.com>
2025-05-26 14:06:11 +0800

    Embedding-3

--- backend/src/main/resources/db/migration/V1_0_1__insert_init_table.sql
+++ backend/src/main/resources/db/migration/V1_0_1__insert_init_table.sql
@@ -37,4 +37,4 @@ INSERT INTO mjga.ai_llm_config (name,code,model_name, type, api_key, url, enable
 VALUES
     ('DeepSeek','DEEP_SEEK','deepseek-chat','CHAT','your_api_key', 'https://api.deepseek.com', false, 0),
     ('智谱清言','ZHI_PU','glm-4-flash','CHAT', 'your_api_key', 'https://open.bigmodel.cn/', false, 1),
-    ('智谱清言向量','ZHI_PU_EMBEDDING','Embeddings-3','EMBEDDING', 'your_api_key', 'https://open.bigmodel.cn/', false, 0);
+    ('智谱清言向量','ZHI_PU_EMBEDDING','Embedding-3','EMBEDDING', 'your_api_key', 'https://open.bigmodel.cn/', false, 0);
